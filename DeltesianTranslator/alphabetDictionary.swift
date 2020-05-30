ab758f9308b19a1c32ede5b5c69647121a3e6354
Liam <ldavidson@lcs.on.ca>
2020-05-30 10:21:30 -0400

    Created dictionaries for al alphabet types.

---
+++ DeltesianTranslator/alphabetDictionary.swift
@@ -0,0 +1,80 @@
+//
+//  alphabetDictionary.swift
+//  DeltesianTranslator
+//
+//  Created by Liam Davidson on 2020-05-30.
+//  Copyright © 2020 Liam Davidson. All rights reserved.
+//
+
+import Foundation
+
+
+//create a dictionary for lowercase consonants
+var consonantLower = [
+    1 : "b",
+    2 : "c",
+    3 : "d",
+    4 : "f",
+    5 : "g",
+    6 : "h",
+    7 : "j",
+    8 : "k",
+    9 : "l",
+    10 : "m",
+    11 : "n",
+    12 : "p",
+    13 : "q",
+    14 : "r",
+    15 : "s",
+    16 : "t",
+    17 : "v",
+    18 : "w",
+    19 : "x",
+    20 : "z",
+]
+
+//create a dictionary for uppercase consonants
+var consonantUpper = [
+    1 : "B",
+    2 : "C",
+    3 : "D",
+    4 : "F",
+    5 : "G",
+    6 : "H",
+    7 : "J",
+    8 : "K",
+    9 : "L",
+    10 : "M",
+    11 : "N",
+    12 : "P",
+    13 : "G",
+    14 : "R",
+    15 : "S",
+    16 : "T",
+    17 : "V",
+    18 : "W",
+    19 : "X",
+    20 : "Z",
+]
+
+//create a dictionary for lowercase vowels
+var vowelLower = [
+    1 : "a",
+    2 : "e",
+    3 : "i",
+    4 : "o",
+    5 : "u",
+    6 : "y",
+]
+
+//create a dictionary for uppercase vowels
+var vowelUpper = [
+    1 : "A",
+    2 : "E",
+    3 : "I",
+    4 : "O",
+    5 : "U",
+    6 : "Y",
+]
+
+
---
+++ DeltesianTranslator/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  DeltesianTranslator
+//
+//  Created by Liam Davidson on 2020-05-29.
+//  Copyright © 2020 Liam Davidson. All rights reserved.
+//
+
+import Foundation
+
+
+//testing how to do user input
+print("Input your text below:")
+var inputTest = "value"
+inputTest = readLine()!
+
+for character in inputTest {
+    let dictionaryToUse = vowelLower
+}
+
+print("You have typed \(inputTest)")
+
