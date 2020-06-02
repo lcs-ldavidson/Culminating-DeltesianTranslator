6c085952fc0db4a51de477285e3db3eb4ab1279d
Liam <ldavidson@lcs.on.ca>
2020-06-01 20:17:57 -0400

    Created a language structure.

---
+++ DeltesianTranslator/Language.swift
@@ -0,0 +1,20 @@
+//
+//  Language.swift
+//  DeltesianTranslator
+//
+//  Created by Liam Davidson on 2020-06-01.
+//  Copyright © 2020 Liam Davidson. All rights reserved.
+//
+
+import Foundation
+
+
+//structure for any given language I might create
+struct language {
+    //name of the language
+    var name : String
+    
+    //shift values
+    var positiveOffsetValue : Int
+    var negativeOffsetValue : Int
+}
