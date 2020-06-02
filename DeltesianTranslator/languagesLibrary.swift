d252e16a6e45bbd9c81eaa9ce866e84a56243203
Liam <ldavidson@lcs.on.ca>
2020-06-02 17:31:22 -0400

    created extensions in Dictionary to get keys and values based on index

--- DeltesianTranslator/languagesLibrary.swift
+++ DeltesianTranslator/languagesLibrary.swift
@@ -10,12 +10,27 @@ import Foundation
 
 //base class for a language
 class language {
+    
+    //universal variables
     var name : String
     var rules : Dictionary<String, String>
     
+    //initializer
     init(name: String, dictionary: Dictionary<String, String>) {
         self.name = name
         self.rules = dictionary
+        
+        //static function to translate a given string into the language
+        func translateToFantasy(_ text: String) -> String {
+            var textToTranslate = text
+            
+            for i in 1...rules.count {
+                textToTranslate.replace(<#T##substring: String##String#>, with: <#T##String#>)
+            }
+            
+        }
+        
     }
 }
+
 
--- DeltesianTranslator/myFunctions.swift
+++ DeltesianTranslator/myFunctions.swift
@@ -9,9 +9,26 @@
 import Foundation
 
 
+//function to replace parts of A STRING WITHOUT MAKING A COPY
 extension String {
     mutating func replace(_ substring: String, with replacement: String) {
         var newString = self.replacingOccurrences(of: substring, with: replacement)
         self = newString
+    }
+}
+
+//get the value from a Dictionary at a specific index
+extension Dictionary {
+    func getValueAtIndex(_ index: Int) -> Value {
+        var indexWithValue = self.index(startIndex, offsetBy: index)
+        return self.values[indexWithValue]
+    }
+}
+
+//get the key at an index
+extension Dictionary {
+    func getKeyAtIndex(_ index: Int) -> Key {
+        var indexWithValue = self.index(startIndex, offsetBy: index)
+        return self.keys[indexWithValue]
     }
 }
