83474753e74c1687d05e01f0911d4b0739b7f1ea
Liam <ldavidson@lcs.on.ca>
2020-06-04 20:06:15 -0400

    Fiddling with a new bug. similar to the lST ONE BUT NOT A APPARENT.

--- DeltesianTranslator/main.swift
+++ DeltesianTranslator/main.swift
@@ -11,7 +11,7 @@ import Foundation
 
 var dwarvenLanguage = language(name: "Dwarven", dictionary: dwarvenRules, shiftValue: 2)
 
-var testText = "My name is Liam. I love fantasy."
+var testText = "When the small town of Ryvon is attacked by a force of savage undead, those not slaughtered are left suffering from a mysterious disease. The lucky few to escape the infection must now race to discover the nature of the disease and the origin of the creatures that brought it. The Archdruids - the magical religious leaders of the town - finally find the answer, but a boy and his father must decide if they are willing to risk the lives of everyone to save the person they love most."
 
 
 var translatedText = dwarvenLanguage.translateToFantasy(testText)
