c1cc886663c2e18a3f3500089b80f7a05c9c2f8c
Liam <ldavidson@lcs.on.ca>
2020-06-04 20:10:10 -0400

    created elven rules. Unedited right now

--- DeltesianTranslator/ruleCharts.swift
+++ DeltesianTranslator/ruleCharts.swift
@@ -40,3 +40,36 @@ var dwarvenRules = [
     " sm" : " v'm",
 ]
 
+
+//rules for elven language
+var elvenRules = [
+    "al " : "urm ",
+    "ive " : "idar ",
+    "est " : "orgur ",
+    "ation " : "adur ",
+    "at " : "idald ",
+    "ed " : "arhald ",
+    "en " : "ur ",
+    "eous " : "odogur ",
+    "er " : "ud ",
+    "es " : "ag'mar ",
+    "ful " : "drak ",
+    "ial " : "ourd ",
+    "ble " : "gred ",
+    "ic " : "yr ",
+    "ing " : "uldran ",
+    "ion " : "ourg ",
+    "ious " : "ikrag ",
+    "y " : "org ",
+    "less " : "grak ",
+    "ment " : "xil ",
+    "ness " : "trag ",
+    "e " : "urg ",
+    " the " : " du'um ",
+    " a " : " da'kal ",
+    " I " : " d'tel ",
+    " of " : " ag'dol ",
+    " is " : " gug'd'maran ",
+    " sm" : " v'm",
+]
+
